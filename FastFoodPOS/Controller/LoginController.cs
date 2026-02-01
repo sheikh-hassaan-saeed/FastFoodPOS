660819139470c3bb111bf39bc7c3fc1615209076
Sheikh Hassaan Saeed <sheikhhassaansaeed@gmail.com>
2026-02-01 18:10:59 +0500

    Controller Folder Add witht the LoginController

---
+++ FastFoodPOS/Controller/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using FastFoodPOS.Common;
+using FastFoodPOS.DAL;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFoodPOS.Controller
+{
+    public class LoginController
+    {
+        //Referencing with DAL to talk to that
+        private LoginDAL _loginDAL = new LoginDAL();
+
+        public bool ExecuteLogin(Admin userObj)
+        {
+            //Contoller will talk to the DAL and DAL will check the credentials
+            return _loginDAL.AuthenticateUser(userObj.Username, userObj.UserPassword);
+        }
+    }
+}
